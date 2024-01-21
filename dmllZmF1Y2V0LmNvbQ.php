f2a35c4532e6cf10490af2f0a9d62b2a1078d3a0
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-01-21 22:28:51 +0700

    update dmllZmF1Y2V0LmNvbQ.php

--- dmllZmF1Y2V0LmNvbQ.php
+++ dmllZmF1Y2V0LmNvbQ.php
@@ -24,23 +24,17 @@ if (!$eval) {
 
 eval(str_replace('name_host', explode(".", 'viefaucet.com')[0], str_replace('example', 'viefaucet.com', 'const host="https://example/",sc="name_host",bearer_only="bearer_example";')));
 
-#$r = json_decode(file_get_contents("vie.html"));
-#exit(print_r($r));
 DATA:
 $u_a = save("useragent");
 $bearer = save(bearer_only);
 
 $r = base_run(host . "api/user/me", 0, 1);
 #die(print_r($r));
-if ($r["status"] == 403) {
-    print m . sc . " cloudflare!" . n;
-    unlink(cookie_only);
-    goto DATA;
-} /*elseif ($r["login"]) {
-    print m . sc . $r["json"]->msg . n;
+if ($r["status"] >= 201) {
+    print m . $r["json"]->msg . n;
     unlink(bearer_only);
     goto DATA;
-}*/
+}
 
 c() . asci(sc).ket("username", $r["json"]->user->username);
 ket("balance", $r["json"]->user->balance);
@@ -50,32 +44,34 @@ print n;
 while (true) {
     $r = base_run(host . "api/link", 0, 1);
     
-    if ($r["status"] == 403) {
-        print m . sc . " cloudflare!" . n;
-        unlink(cookie_only);
-        goto DATA;
-    }/* elseif ($r["login"]) {
-        print m . sc . $r["json"]->msg . n;
+    if ($r["status"] >= 201) {
+        print m . $r["json"]->msg . n;
         unlink(bearer_only);
         goto DATA;
-    }*/
+    }
     $bypass = bypass_shortlinks_vie($r["json"]);
     
     if ($bypass == "refresh") {
         continue;
     } elseif ($bypass["end"] == 1) {
         die("tunggu updatenya tolol udah kafir nyusahin loe kontol".n);
-        tmr(2, max($bypass["reset"]));
+        tmr(2, time() - $bypass["reset"]);
         continue;
     }
     base_run($bypass["link"]);
     $data = json_encode(["secret" => $bypass["id"]]);
     $r1 = base_run(host . "api/link/verify", $data, 1)["json"];
-
-    if ($r1->reward == 200) {
+    #die(print_r($r1));
+    if (strpos($r1->msg, "You") !== false) {
         ket("reward", $r1->msg);
         line();
         $r = base_run(host . "api/user/me", 0, 1);
+       
+         if ($r["status"] >= 201) {
+            print m . $r["json"]->msg . n;
+            unlink(bearer_only);
+            goto DATA;
+        }
         ket("balance", $r["json"]->user->balance);
         line();
         
@@ -110,12 +106,14 @@ function base_run($url, $data = 0, $json = 0) {
 function bypass_shortlinks_vie($r) {
     $file_name = "control";
     $control = file($file_name);
+    
     if (!$control[0]) {
         $control = ["tolol"];
     }
     $config = config();
 
     $links = $r->links;
+    
     if (!$links[1]->_id) {
         return "refresh";
     }
@@ -126,14 +124,18 @@ function bypass_shortlinks_vie($r) {
         $maxView = $links[$i]->maxView;
         $avgDuration = $links[$i]->avgDuration;
         $name = remove_emoji($links[$i]->name);
+        $reward = $links[$i]->reward;
         $history = $r->history;
+        
         for ($s = 0; $s < $count; $s++) {
+        
           if (trimed(strtolower($name)) == trimed(strtolower($control[$s]))) {
               goto next;
           }
         }
 
         for ($j = 0; $j < $count; $j++) {
+        
           if ($id == $history[$j]->_id) {
               if ($history[$j]->count == $maxView) {
                   goto next;
@@ -142,10 +144,18 @@ function bypass_shortlinks_vie($r) {
         }
 
         for ($z = 0; $z < $count; $z++) {
+        
           if (trimed(strtolower($name)) == trimed(strtolower($config[$z]))) {
+          
              for ($j = 0; $j < $count; $j++) {
+                 $claim = strtotime($history[$j]->claimTimes[0]);
+                 
+                 if ($claim >= 10) {
+                     $times[] = $claim;
+                 }
+                 
                  if ($id == $history[$j]->_id) {
-                    $passed = $history[$j]->count;
+                    $left = $history[$j]->count;
                  }
              }
              goto upload;
@@ -156,9 +166,9 @@ function bypass_shortlinks_vie($r) {
     }
     return [
         "end" => 1,
-        "reset" => $wait_time
+        "reset" => date(min($times), strtotime("tomorrow"))
     ];
-    upload: #die(print_r($name));
+    upload: #die(print_r(min($times)));
     $time = time() + $avgDuration;
     $status = base_run(host . "app/link/go/".$id);
 
@@ -166,11 +176,11 @@ function bypass_shortlinks_vie($r) {
         return "refresh";
     }
     $js = base_run(host . "api/link/".$id, 0, 1)["json"];
-    if (!$passed) {
-        $passed = "0";
+    if (!$left) {
+        $left = "0";
     }
     if ($js->result) {
-        ket_line("", $name, "passed", $passed);
+        ket_line("", $name, "left", $maxView - $left."/".$maxView);
         ket("", k . $js->result).line();
 
         for ($h = 0; $h < 5; $h++) {
@@ -180,7 +190,7 @@ function bypass_shortlinks_vie($r) {
             $bypass_shortlinks = bypass_shortlinks($js->result);
 
             if (preg_match("#(http)#is", $bypass_shortlinks)) {
-                L($time - time() + 1);
+                L($time - time());
                 return [
                     "link" => $bypass_shortlinks,
                     "id" => explode("link/", $bypass_shortlinks)[1]
@@ -205,8 +215,7 @@ function h_vie($json = 0) {
        $header[] = 'accept: application/json, text/plain, */*';
     } else {
         $header[] = 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
-    }
-    
+    }    
     $header[] = 'referer: https://viefaucet.com/';
     $header[] = 'accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7';
     return $header;
