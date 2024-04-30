9daaa9504b1a7fba9a07305649ff825e7f293d82
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-04-30 22:03:59 +0700

    update Wl9CYXNlNjQ.php

--- Wl9CYXNlNjQ.php
+++ Wl9CYXNlNjQ.php
@@ -1,8 +1,26 @@
 <?php
 
+if (!$eval) {
+    eval(str_replace('<?php', "", get_e("build_index.php")));
+    $reques = array(
+        1 => "xevil", 2 => "multibot"
+    );
+    ket(1, "xevil", 2, "multibot");
+    
+    while(true) {
+        $inp = tx("number", 1);
+      
+        if ($inp == 0) {
+            continue;
+        } elseif (2 >= $inp) {
+            break;
+        }
+    }
+    eval(str_replace('seconds = 90', 'seconds = 5', str_replace('<?php',"", str_replace("mode_proxy", $method_proxy, str_replace("request_captcha", $reques[$inp], get_e("shortlink_index.php"))))));
+}
 memek:
 if (!file_get_contents("link_sl.php")) {
-    print p.base64_decode("ZmlsZSBpbmNsdWRlIHRpZGFrIGRpIHRlbXVrYW4gYnVhdGxhaCBmaWxlIChsaW5rX3NsLnBocCkgdG9sb2wKCmNvbnRvaDoKCjxwaHAKCiRhcnJheSA9IGFycl9yYW5kKFsKICAgIFsicmV2Y3V0Lm5ldCIsICJhcGlrZXltdSJdLAogICAgWyJjdXRsaW5rLnh5eiIsICJhcGlrZXltdSJdLAogICAgWyJiaXRhZC5vcmciLCAiYXBpa2V5bXUiXSwKICAgIFsidXJsY3V0LnBybyIsICJhcGlrZXltdSJdLAogICAgWyJmYWhvLnVzIiwgImFwaWtleW11Il0sCiAgICBbImlubGlua3Mub25saW5lIiwgImFwaWtleW11Il0KXSk7").n;
+    print p.base64_decode("PD9waHAgJGxpbmsgPSBhcnJheSgpOwkbGlzdF9hcnJheSA9IFsNCgkJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJXTsNCg==").n;
     tx("enter to continue");
     c();
     goto memek;
@@ -18,6 +36,13 @@ while ($x <= count($array)) {
     
     if (!$array[$x][1]) {
         break;
+    }
+    
+    foreach ($list_array as $index => $item) {
+        if ($item[0] == $array[$x][0]) {
+            $id = $index + 10001;
+            break;
+        }
     }
     $hasil = curl("https://".$array[$x][0]."/api?api=".$array[$x][1]."&url=".urlencode('https://autofaucet.org/dashboard/shortlinks/visited/'.az_num(rand(10, 32))))[2];
     
@@ -38,8 +63,16 @@ while ($nomor <= count($url)) {
     }
     print k."host: ".$array[$nomor][0].n;
     print k."link: ".$only_sl.n;
+    $n = 0;
+    b:
+    $n++;
     $r = bypass_shortlinks($only_sl);
-
+    
+    if ($r->status !== "success") {
+        if (2 >= $n) {
+            goto b;
+        }
+    }
     if ($r->status == "success") {
         print_r($r);
     } else {
