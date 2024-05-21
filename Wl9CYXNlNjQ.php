a93e564a907cb8fbe2963da1f56db0a51ba7178a
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-05-21 20:06:31 +0700

    update Wl9CYXNlNjQ.php

--- Wl9CYXNlNjQ.php
+++ Wl9CYXNlNjQ.php
@@ -19,56 +19,62 @@ if (!$eval) {
     eval(str_replace('seconds = 90', 'seconds = 5', str_replace('<?php',"", str_replace("mode_proxy", $method_proxy, str_replace("request_captcha", $reques[$inp], get_e("shortlink_index.php"))))));
 }
 define('bypassed', 'bypassed');
+$name_file = "link_sl.php";
 memek:
-if (!file_get_contents("link_sl.php")) {
-    print p.base64_decode("PD9waHAgJGxpbmsgPSBhcnJheSgpOwkbGlzdF9hcnJheSA9IFsNCgkJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJWyJkb21haW4iLCAiYXBpa2V5Il0sDQoJXTsNCg==").n;
+
+if (!file_get_contents($name_file)) {
+    print p.base64_decode("aHR0cHM6Ly9leGFtcGxlLmNvbS9hcGk/YXBpPWFwaWtleSZ1cmw9eW91cmRlc3RpbmF0aW9ubGluay5jb20KaHR0cHM6Ly9leGFtcGxlLnh5ei9hcGk/YXBpPWFwaWtleSZ1cmw9eW91cmRlc3RpbmF0aW9ubGluay5jb20mYWxpYXM9Q3VzdG9tQWxpYXMmZm9ybWF0PXRleHQ=").n;
     tx("enter to continue");
     c();
     goto memek;
 }
-include("link_sl.php");
-if (!$array[0][1] || !$array[0][0]) {
-    unlink("link_sl.php");
+$array_file = file($name_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$array = arr_rand($array_file);
+if (strpos($array[0], "api") === false) {
+    unlink($name_file);
     goto memek;
 }
-$x = 0;
-while ($x <= count($array)) {
 
+$x = 0;
+while ($x <= count($array) + 1) {
+
+    if (strpos($array[$x], "#") !== false) {
+       continue;
+    }
     
-    if (!$array[$x][1]) {
+    if (!$array[$x]) {
         break;
     }
+    $short_url = explode("your", $array[$x])[0];
+    $parsed_url = parse_url($array[$x]);
     
-    foreach ($list_array as $index => $item) {
-        if ($item[0] == $array[$x][0]) {
-            $id = $index + 10001;
-            break;
-        }
-    }
-    $hasil = curl("https://".$array[$x][0]."/api?api=".$array[$x][1]."&url=".urlencode('https://autofaucet.org/dashboard/shortlinks/visited/'.az_num(rand(10, 32))))[2];
+    $redirect_url = 'https://autofaucet.org/dashboard/shortlinks/visited/'.az_num(rand(10, 32));
+    $hasil = curl($short_url.urlencode($redirect_url))[2];
     
     if($hasil->status == "success") {
+        $host[] = $parsed_url['host'];
         $link[] = $hasil->shortenedUrl;
+    } else {
+        print $array[$x][0].n;
     }
     $x++;
 }
 
-$url = array_filter($link);
+$only_sl = array_filter($link);
 $nomor = 0;
-while ($nomor <= count($url)) {
-    $only_sl = trimed($url[$nomor]);
-    
-    if (!$only_sl) {
+while ($nomor <= count($only_sl) + 1) {
+
+    if (!$only_sl[$nomor]) {
         unset($link);
         goto memek;
     }
-    print k."host: ".$array[$nomor][0].n;
-    print k."link: ".$only_sl.n;
+    print k."host: ".$host[$nomor].n;
+    print k."link: ".$only_sl[$nomor].n;
     $n = 0;
     b:
     $n++;
-    $r = bypass_shortlinks($only_sl);
-    
+    $r = bypass_shortlinks($only_sl[$nomor]);
+
     if ($r->status !== "success") {
         if (0 >= $n) {
             goto b;
