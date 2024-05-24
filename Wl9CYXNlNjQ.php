9f0e591422c043cbe2a9c38ed244ed5906e4c260
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-05-24 20:52:23 +0700

    update Wl9CYXNlNjQ.php

--- Wl9CYXNlNjQ.php
+++ Wl9CYXNlNjQ.php
@@ -48,11 +48,10 @@ while ($x <= count($array) + 1) {
     $short_url = explode("your", $array[$x])[0];
     $parsed_url = parse_url($array[$x]);
     
-    if ($link_perkontol) {
-        $redirect_url = $link_perkontol;
+    if (file_get_contents("link_perkontol")) {
+        $redirect_url = arr_rand(file($name_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES))[0];
     } else {
-        $redirect_url = 'https://caesparta.blogspot.com/2013/07/alasan-kenapa-akun-ga-anda-dinonaktifkan.html?m=1';
-        //$redirect_url = 'https://autofaucet.org/dashboard/shortlinks/visited/'.az_num(rand(10, 32));
+        $redirect_url = 'https://autofaucet.org/dashboard/shortlinks/visited/'.az_num(rand(10, 32));
     }
     $hasil = curl($short_url.urlencode($redirect_url))[2];
     $host = [];
