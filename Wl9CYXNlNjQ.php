35e5bbbbb8e0d3976cf86fcca4cb350260410971
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-06-22 18:01:03 +0700

    update Wl9CYXNlNjQ.php

--- Wl9CYXNlNjQ.php
+++ Wl9CYXNlNjQ.php
@@ -91,10 +91,15 @@ while ($nomor <= count($only_sl) + 1) {
 
     if ($r->status == "success" || 6 >= strpos($r, "//")) {
         print_r($r);
+    } elseif ($r->status !== "success") {
+        if (0 >= $n) {
+            goto b;
+        }
     } else {
         print m."invalid!".n;
     }
     line();
     $nomor++;
 }
-goto memek;
+
+
