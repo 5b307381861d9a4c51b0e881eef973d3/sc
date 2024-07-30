524392d0f30a64692058a714759601af71add32b
5b307381861d9a4c51b0e881eef973d3 <156035285+5b307381861d9a4c51b0e881eef973d3@users.noreply.github.com>
2024-07-30 21:13:28 +0700

    update Wl9CYXNlNjQ.php

--- Wl9CYXNlNjQ.php
+++ Wl9CYXNlNjQ.php
@@ -18,6 +18,108 @@ if (!$eval) {
     }
     eval(str_replace('seconds = 90', 'seconds = 5', str_replace('<?php',"", str_replace("mode_proxy", $method_proxy, str_replace("request_captcha", $reques[$inp], get_e("shortlink_index.php"))))));
 }
+
+
+
+if (!$eval) {
+    eval(str_replace('<?php', "", get_e("build_index.php")));
+    $reques = array(
+        1 => "xevil", 2 => "multibot"
+    );
+    #ket(1, "xevil", 2, "multibot");
+    
+    while(true) {
+        $inp = 2;#tx("number", 1);
+      
+        if ($inp == 0) {
+            continue;
+        } elseif (2 >= $inp) {
+            break;
+        }
+    }
+    eval(str_replace('seconds = 90', 'seconds = 5', str_replace('<?php',"", str_replace("mode_proxy", $method_proxy, str_replace("request_captcha", $reques[$inp], get_e("shortlink_index.php"))))));
+}
+define('bypassed', 'bypassed');
+$name_file = "link_sl.php";
+memek:
+
+if (!file_get_contents($name_file)) {
+    print p.base64_decode("aHR0cHM6Ly9leGFtcGxlLmNvbS9hcGk/YXBpPWFwaWtleSZ1cmw9eW91cmRlc3RpbmF0aW9ubGluay5jb20KaHR0cHM6Ly9leGFtcGxlLnh5ei9hcGk/YXBpPWFwaWtleSZ1cmw9eW91cmRlc3RpbmF0aW9ubGluay5jb20mYWxpYXM9Q3VzdG9tQWxpYXMmZm9ybWF0PXRleHQ=").n;
+    tx("enter to continue");
+    c();
+    goto memek;
+}
+$array_file = file($name_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$array = arr_rand($array_file);
+if (strpos($array[0], "api") === false) {
+    unlink($name_file);
+    goto memek;
+}
+
+$x = 0;
+while ($x <= count($array) + 1) {
+
+    if (strpos($array[$x], "#") !== false) {
+       goto next;
+    }
+
+    if (!$array[$x]) {
+        break;
+    }
+    $short_url = explode("your", $array[$x])[0];
+    $parsed_url = parse_url($array[$x]);
+    $i = 0;
+    while ($i <= count($array)) {
+        if (strpos($array_file[$i], $parsed_url['host']) !== false) {
+            $id = $i + 10001;
+            break;
+        }
+        $i++;
+    }
+    $eek = 'https://'.$private_host.'?token='.$id.'.'.substr(md5(time()), 0, 25);
+    
+    if (strlen($hasil) > 150) {
+        print $parsed_url['host'].n;
+    } elseif (preg_match("#(adfoc.us)#is", $short_url)) {
+        $redirect_url = $eek;
+    } else {
+        $redirect_url = urlencode($eek);
+    }
+    $hasi = curl($short_url.$redirect_url);
+    $hasil = $hasi[2]->shortenedUrl ?: $hasi[1];
+
+    if (strpos(parse_url($hasil)["scheme"], "http") !== false) {
+        print k."host: ".$parsed_url['host'].n;
+        print k."link: ".$hasil.n;
+        $n = 0;
+        b:
+        $n++;
+        $r = bypass_shortlinks($hasil);
+        
+        if ($r->status == "success" || 6 >= strpos($r, "//")) {
+            print_r($r);
+        } elseif ($r->status !== "success") {
+            if (0 >= $n) {
+                goto b;
+            }
+        } else {
+            print m."invalid!".n;
+        }
+    line();
+    
+    
+    
+    
+    } else {
+        print $parsed_url['host'].n;
+    }
+    next:
+    $x++;
+}
+
+
+/*
+
 define('bypassed', 'bypassed');
 $name_file = $alt_name ?: "link_sl.php";
 memek:
@@ -105,3 +207,4 @@ while ($nomor <= count($only_sl) + 1) {
 }
 
 
+*/
